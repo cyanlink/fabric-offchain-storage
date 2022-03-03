8484cd6058f2b95b165c80f95a8e3b3302d6dcc8
cyanlink <tgvita@outlook.com>
2022-03-03 13:41:50 +0800

    protobuf generate

--- app.go
+++ app.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
 
+	"google.golang.org/grpc"
+	pb "my-fabric-offchain-storage/query"
+	// api "my-fabric-offchain-storage/api"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedQueryProcessorServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(_ context.Context, in *pb.QueryRequest) (*pb.QueryResponse, error) {
+	log.Printf("Received: %v", in)
+	return &pb.QueryResponse{}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterQueryProcessorServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
